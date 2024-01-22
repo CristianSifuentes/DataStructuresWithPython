92a7e901812ef334f1ba74c94ed53b8aab4258bd
CristianSifuentes <sifuentescovarrubiasitsl@gmail.com>
2024-01-22 17:42:09 -0600

    Adding another column for practice

--- ConditionalNestingList.py
+++ ConditionalNestingList.py
@@ -5,11 +5,13 @@
 
 # Lista de listas con productos y precios
 productos_precios = [
-    ["Laptop", 1200, False],
-    ["Smartphone", 800, True],
-    ["Tablet", 400, True],
-    ["Cámara", 300, False],
-    ["Auriculares", 100, True]
+    ["Laptop", 1200, False, False, 'C'],
+    ["Smartphone", 800, True, True, 'C'],
+    ["Tablet", 400, True, True,  'C'],
+    ["Cámara", 300, False, False, 'C'],
+    ["Auriculares", 100, True, False, 'C'],
+    ["Sofa", 100, True, False, 'M'],
+    ["Cama", 100, True, False, 'M']
 ]
 
 # Comprensión de lista anidada con condicionales
@@ -19,8 +21,16 @@ productos_con_descuento = [
     if producto[1] > 500 and producto[2]  # Aplicamos condiciones: precio > 500 y descuento aplicado
 ]
 
+productos_en_existencia = [
+    producto[0]
+    for producto in productos_precios
+    if producto[3] == True 
+]
+
+
 # Imprimimos los resultados
 print("Productos con descuento:", productos_con_descuento)
+print("Productos en existencia:", productos_en_existencia)
 
 
 '''
