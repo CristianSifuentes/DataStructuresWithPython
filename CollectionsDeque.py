a8a93df047183d8e372c0e681643a80df90105c7
CristianSifuentes <sifuentescovarrubiasitsl@gmail.com>
2024-02-01 11:25:32 -0600

    Comments

--- CollectionsDeque.py
+++ CollectionsDeque.py
@@ -1,3 +1,7 @@
+'''
+Claro, vamos a crear un ejemplo avanzado utilizando pilas implementadas con collections.deque. Supongamos que estamos construyendo un sistema de navegación para una aplicación, y queremos gestionar el historial de páginas visitadas.
+'''
+
 from collections import deque
 
 # Definimos una pila (deque) para el historial de páginas visitadas
@@ -30,3 +34,15 @@ print(f"\nRetrocediendo a la página anterior: {pagina_retrocedida}")
 print("\nHistorial de páginas visitadas después de retroceder:")
 for pagina in historial_paginas:
     print(pagina)
+
+
+'''
+En este ejemplo:
+
+Utilizamos collections.deque para implementar una pila que representa el historial de páginas visitadas.
+Limitamos el tamaño del historial a 5 páginas usando el argumento maxlen de deque.
+Simulamos la navegación a diversas páginas y las agregamos al historial.
+Mostramos el historial de páginas visitadas.
+Simulamos retroceder en el historial utilizando el método pop de deque.
+Este ejemplo ilustra cómo una pila implementada con collections.deque puede ser utilizada para gestionar el historial de una manera eficiente, y cómo se pueden aprovechar sus características, como el límite de tamaño automático, en el desarrollo diario de una aplicación.
+'''
