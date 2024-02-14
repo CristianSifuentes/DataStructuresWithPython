c2cb33ada78c3a1454039efc3d69fa7af1e60f4b
CristianSifuentes <sifuentescovarrubiasitsl@gmail.com>
2024-02-14 17:49:29 -0600

    Creating OOP program using trees

---
+++ TreeOOP.py
@@ -0,0 +1,82 @@
+class NodoArbol:
+    def __init__(self, valor):
+        self.valor = valor
+        self.izquierda = None
+        self.derecha = None
+
+class ArbolBinarioBusqueda:
+    def __init__(self):
+        self.raiz = None
+
+    def insertar(self, valor):
+        if self.raiz is None:
+            self.raiz = NodoArbol(valor)
+        else:
+            self._insertar_recursivo(self.raiz, valor)
+
+    def _insertar_recursivo(self, nodo, valor):
+        if valor < nodo.valor:
+            if nodo.izquierda is None:
+                nodo.izquierda = NodoArbol(valor)
+            else:
+                self._insertar_recursivo(nodo.izquierda, valor)
+        elif valor > nodo.valor:
+            if nodo.derecha is None:
+                nodo.derecha = NodoArbol(valor)
+            else:
+                self._insertar_recursivo(nodo.derecha, valor)
+        else:
+            # Valor ya existe en el árbol, no hacemos nada
+            pass
+
+    def buscar(self, valor):
+        return self._buscar_recursivo(self.raiz, valor)
+
+    def _buscar_recursivo(self, nodo, valor):
+        if nodo is None:
+            return False
+        elif valor == nodo.valor:
+            return True
+        elif valor < nodo.valor:
+            return self._buscar_recursivo(nodo.izquierda, valor)
+        else:
+            return self._buscar_recursivo(nodo.derecha, valor)
+
+    def eliminar(self, valor):
+        self.raiz = self._eliminar_recursivo(self.raiz, valor)
+
+    def _eliminar_recursivo(self, nodo, valor):
+        if nodo is None:
+            return nodo
+        if valor < nodo.valor:
+            nodo.izquierda = self._eliminar_recursivo(nodo.izquierda, valor)
+        elif valor > nodo.valor:
+            nodo.derecha = self._eliminar_recursivo(nodo.derecha, valor)
+        else:
+            if nodo.izquierda is None:
+                return nodo.derecha
+            elif nodo.derecha is None:
+                return nodo.izquierda
+            nodo.valor = self._encontrar_minimo(nodo.derecha)
+            nodo.derecha = self._eliminar_recursivo(nodo.derecha, nodo.valor)
+        return nodo
+
+    def _encontrar_minimo(self, nodo):
+        while nodo.izquierda:
+            nodo = nodo.izquierda
+        return nodo.valor
+
+# Creamos un árbol binario de búsqueda
+arbol = ArbolBinarioBusqueda()
+arbol.insertar(10)
+arbol.insertar(5)
+arbol.insertar(15)
+arbol.insertar(3)
+arbol.insertar(7)
+arbol.insertar(12)
+arbol.insertar(18)
+
+# Realizamos algunas operaciones
+print(arbol.buscar(7))  # True
+arbol.eliminar(15)
+print(arbol.buscar(15))  # False
